6ec174c58f601d14d645f520dfb9a2acc975e32d
AshokMadagoni <madagoniashok1@gmail.com>
2025-02-02 00:18:11 +0530

    updated all files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/react-day2/', // Change this to match your GitHub repo name
   plugins: [react()],
-  basename:"/react-day2"
-})
+});
